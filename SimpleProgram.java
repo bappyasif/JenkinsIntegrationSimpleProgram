16d39ae68e3de683b1e95d34bc53d7dc1acecf5e
bappyasif <bappyasif@hotmail.com>
2019-04-23 21:24:30 +0600

    First Commit to this Simple Java Program to Test Integration Using Jenkins from CMD

---
+++ SimpleProgram.java
@@ -0,0 +1,15 @@
+public class SimpleProgram{
+	
+	public static void main(String[] args){
+		
+		int count = 0;
+		do {
+			
+			System.out.println("Hello World " + "Printed " +count);
+			count++;
+			
+		} while (count < 10);
+		
+	}
+	
+}
